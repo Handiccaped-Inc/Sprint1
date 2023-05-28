3c4e9740f0878116a739df4245b825c3828790b3
SantiagoAV <81932949+SantiAV25@users.noreply.github.com>
2023-05-28 00:52:19 -0500

    add scriptodatabse and corrected databse

--- Creacion de base de datos/database.sql
+++ Creacion de base de datos/database.sql
@@ -30,9 +30,16 @@ create table rol(
     constraint pk_rol primary key(rol_id)
 );
 
+create table category(
+    category_id integer not null,
+    category_name text not null,
+    constraint pk_category primary key (category_id)
+);
+
 create table product(
     product_id integer not null,
     user_id integer not null,
+    category_id integer not null,
     product_name text not null,
     product_description text not null,
     product_price real not null,
@@ -40,21 +47,29 @@ create table product(
     product_latitude integer not null,
     product_length integer not null,
     constraint pk_product primary key (product_id),
-    constraint fk_product_user FOREIGN KEY (user_id) REFERENCES user(user_id)
+    constraint fk_product_user FOREIGN KEY (user_id) REFERENCES user(user_id),
+    constraint fk_product_category FOREIGN KEY (category_id) REFERENCES category(category_id)
 );
 
 CREATE TABLE orders(
-  orders_id INTEGER not null,
+  orders_id INTEGER not null, ----Incrementativa
   user_id INTEGER not null,
   product_id INTEGER not null,
-  orders_deliver_date TEXT not null,
-  orders_recipient_name TEXT not null,
-  orders_status TEXT,
+  orders_status TEXT not null,
   orders_price REAL not null,
-  orders_user_id INTEGER,
-  orders_product_id INTEGER,
   constraint pk_orders primary key (orders_id),
-  constraint fk_orders_user FOREIGN KEY (user_id) REFERENCES user(id),
-  constraint fk_orders_product FOREIGN KEY (product_id) REFERENCES product(id),
+  constraint fk_orders_user FOREIGN KEY (user_id) REFERENCES user(user_id),
+  constraint fk_orders_product FOREIGN KEY (product_id) REFERENCES product(product_id),
   constraint ckc_status CHECK (orders_status IN ('entregado','cancelado','en espera'))
+);
+
+create table delivery(
+    delivery_id integer not null, ----Incrementativa
+    orders_id integer not null,
+    repartidor_id integer not null,
+    delivery_receptor text not null,
+    delivery_date date not null,
+    constraint pk_delivery primary key (delivery_id),
+    constraint fk_delivery_orders FOREIGN KEY (orders_id) REFERENCES orders(orders_id),
+    constraint fk_delivery_user FOREIGN KEY (repartidor_id) REFERENCES user(user_id)
 );
