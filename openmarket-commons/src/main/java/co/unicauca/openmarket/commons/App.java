b90495ffaceac1d2ab150434fbf27dd4a2ad0309
Jojan-Esteban-Serna <78891995+Jojan-Esteban-Serna@users.noreply.github.com>
2023-05-29 23:10:37 -0500

    Added the openmarket-commons base project

---
+++ openmarket-commons/src/main/java/co/unicauca/openmarket/commons/App.java
@@ -0,0 +1,13 @@
+package co.unicauca.openmarket.commons;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
