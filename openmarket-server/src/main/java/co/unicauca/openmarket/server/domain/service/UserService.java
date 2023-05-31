12d01fa81051e366e9c30b6a09c255ea8afd35b2
BraianRey <braianreyc@unicauca.edu.co>
2023-05-31 01:49:07 -0500

    UserService
    Add UserService and IUserService

---
+++ openmarket-server/src/main/java/co/unicauca/openmarket/server/domain/service/UserService.java
@@ -0,0 +1,24 @@
+package co.unicauca.openmarket.server.domain.service;
+
+import co.unicauca.openmarket.commons.domain.User;
+import co.unicauca.openmarket.server.access.IUserRepository;
+
+public class UserService implements IUserService {
+
+    private static IUserRepository repository;
+
+    public UserService(IUserRepository repository){
+        this.repository = repository;
+    }
+
+    @Override
+    public User findByEmailAndPassword(String email, String password) {
+        return repository.findByEmailAndPassword(email, password);
+    }
+
+    @Override
+    public User findById(Long userId) {
+        return repository.findById(userId);
+    }
+    
+}
