11f2f509e7b8112659e0957819e1902eeafb36ab
SantiagoAV <81932949+SantiAV25@users.noreply.github.com>
2023-05-31 00:57:39 -0500

    add delivery service

---
+++ openmarket-server/src/main/java/co/unicauca/openmarket/server/domain/service/IDeliveryService.java
@@ -0,0 +1,26 @@
+package co.unicauca.openmarket.server.domain.service;
+
+import co.unicauca.openmarket.commons.domain.Delivery;
+
+/**
+ * Interface que maneja los metodos que deben
+ * contener todos los Service de que desean manejar un pedido
+ */
+public interface IDeliveryService {
+
+      /**
+     * Metodo que maneja el guardar un nuevo pedido
+     * @param newDelivery objeto que contiene los datos de un pedido
+     * @return boolean dependiendo del exito de la operación 
+     */
+    public String save(Delivery newDelivery);
+
+    /**
+     * Metodo que maneja el actualizar un pedido
+     * @param newDelivery objeto que cotiene los datos de un pedido
+     * @return boolean dependiendo del exito de la operación
+     */
+
+    public String update(Delivery newDelivery);
+    
+}
