960f66b373e902019e08b9b903e929318ca2e000
SantiagoAV <81932949+SantiAV25@users.noreply.github.com>
2023-05-30 20:57:54 -0500

    add new methos IorderRepository

--- openmarket-server/src/main/java/co/unicauca/openmarket/server/access/IOrderRepository.java
+++ openmarket-server/src/main/java/co/unicauca/openmarket/server/access/IOrderRepository.java
@@ -1,9 +1,38 @@
 package co.unicauca.openmarket.server.access;
 
+import java.util.List;
+
 import co.unicauca.openmarket.commons.domain.Order;
+import co.unicauca.openmarket.commons.domain.StatusOrder;
 
 public interface IOrderRepository {
+    /**
+     * @brief Metodo para guardar una orden
+     * @param NewOrder objeto de esquema de una orden
+     * @retunr boolean deacuerdo a resulado
+     */
     public boolean save(Order newOrder);
+    /**
+     * @brief Metodo para actualizar una orden
+     * @param NewOrder objeto de esquema de una orden
+     * @retunr boolean deacuerdo a resulado
+     */
 
     public boolean update(Order newOrder);
+
+    /**
+     * @brief Metodo que encuentra la orden por el estado de esta
+     * @param status objet que contiene el estaod de una orden
+     * @return lista de objetos entcontrados
+     */
+
+    public List<Order> finByState(StatusOrder status);
+
+    /**
+     * @brief Metodo que encuentra la orden el id del usuario
+     * @param UserId id del usuario
+     * @return lista de objetos entcontrados
+     */
+    public List<Order> finByUser(Integer UserId);
+
 }
