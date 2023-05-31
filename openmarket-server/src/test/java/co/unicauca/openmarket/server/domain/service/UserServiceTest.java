06112b265f29629741ec42586363dfacd2f2e1b0
Jojan-Esteban-Serna <78891995+Jojan-Esteban-Serna@users.noreply.github.com>
2023-05-31 03:24:53 -0500

    Fixed the pom errors and test location folder

--- openmarket-server/src/test/java/co/unicauca/openmarket/server/domain/service/UserServiceTest.java
+++ openmarket-server/src/test/java/co/unicauca/openmarket/server/domain/service/UserServiceTest.java
@@ -1,4 +1,5 @@
-import java.beans.Transient;
+package co.unicauca.openmarket.server.domain.service;
+
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,53 +12,54 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import co.unicauca.openmarket.commons.domain.Rol;
 import co.unicauca.openmarket.commons.domain.User;
 import co.unicauca.openmarket.server.access.IUserRepository;
-import co.unicauca.openmarket.server.domain.service.UserService;
 
-public class userServiceTest {
+public class UserServiceTest {
 
     private UserService service;
     private IUserRepository repository;
 
-    public userServiceTest(){
+    public UserServiceTest() {
         repository = new MockUserServiceRepository();
         service = new UserService(repository);
     }
 
     @Test
-    public void testFindbyemailandpasswordSucces(){
+    public void testFindbyemailandpasswordSucces() {
         User user = service.findByEmailAndPassword("example@example.com", "password123");
         assertNotNull(user);
-        assertEquals(1l,user.getId());
+        assertEquals(1l, user.getId());
     }
 
     @Test
-    public void testFindbyemailandpasswordFaildEmail(){
+    public void testFindbyemailandpasswordFaildEmail() {
         User user = service.findByEmailAndPassword("", "password123");
         assertNull(user);
     }
 
     @Test
-    public void testFindbyemailandpasswordFaildPassword(){
+    public void testFindbyemailandpasswordFaildPassword() {
         User user = service.findByEmailAndPassword("example2@example2.com", "");
         assertNull(user);
     }
 
-
-    private class MockUserServiceRepository implements IUserRepository{
+    private class MockUserServiceRepository implements IUserRepository {
         List<User> usaurios;
-        public MockUserServiceRepository(){
+
+        public MockUserServiceRepository() {
             this.usaurios = new ArrayList<>();
-            User user = new User(1L, new Rol(1L,"Vendedor"),new Date(0), "example@example.com","1234567890", "1234 5678 9012 3456", 
-            "Jojan Esteban", "jojanE", "password123", "123 Street, City" );
-            User user2 = new User(2L, new Rol(2l,"Compradro"),new Date(0), "example2@example2.com","1234567890", "1234 5678 9012 3456", 
-            "pablo ruiz", "pabloR", "password456", "134 Street, City" );
+            User user = new User(1L, new Rol(1L, "Vendedor"), new Date(0), "example@example.com", "1234567890",
+                    "1234 5678 9012 3456",
+                    "Jojan Esteban", "jojanE", "password123", "123 Street, City");
+            User user2 = new User(2L, new Rol(2l, "Compradro"), new Date(0), "example2@example2.com", "1234567890",
+                    "1234 5678 9012 3456",
+                    "pablo ruiz", "pabloR", "password456", "134 Street, City");
             usaurios.add(user2);
             usaurios.add(user);
         }
 
-        public User findByEmailAndPassword(String email, String password){
+        public User findByEmailAndPassword(String email, String password) {
             for (User user : usaurios) {
-                if(email == user.getEmail() && password == user.getPassword()){
+                if (email == user.getEmail() && password == user.getPassword()) {
                     return user;
                 }
             }
@@ -66,15 +68,15 @@ public class userServiceTest {
 
         }
 
-        public User findById(Long userId){
+        public User findById(Long userId) {
             for (User user : usaurios) {
-                if(userId == user.getId()){
+                if (userId == user.getId()) {
                     return user;
                 }
             }
             return null;
         }
-        
+
     }
-    
+
 }
