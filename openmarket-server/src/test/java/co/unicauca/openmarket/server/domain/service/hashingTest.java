ff153ff575e6756bbce2690d0da8162142a85fbe
SantiagoAV <81932949+SantiAV25@users.noreply.github.com>
2023-05-31 03:39:59 -0500

    feature/Santiago_Agredo/add-hashing-Test

---
+++ openmarket-server/src/test/java/co/unicauca/openmarket/server/domain/service/hashingTest.java
@@ -0,0 +1,41 @@
+package co.unicauca.openmarket.server.domain.service;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.Test;
+import co.unicauca.openmarket.server.infra.Hashing;
+public class hashingTest {
+
+    
+
+    @Test
+    public void HashTestConMinuMayus(){
+        String hashfrase = Hashing.getSHA256Hash("HolaComoEstas");
+        String fraseHasheada = "9b01572420f52a4c3ee8973b7abd0eed2b6bee1ad5652f25fac9aae5ed764feb";
+        assertEquals(hashfrase,fraseHasheada);
+    }
+
+    @Test
+    public void hashtTestconMinMayusNums(){
+        String hashfrase = Hashing.getSHA256Hash("HolaC0moEst4s");
+        String fraseHasheada = "1bbec870f9aeb1595ca6fcf09968cb174fdcfa42120526814601f09718b54af0";
+        assertEquals(hashfrase,fraseHasheada);
+    }
+
+    @Test
+    public void hastTestConMinMayusNumsSibols(){
+        String hashfrase = Hashing.getSHA256Hash("!HolaC0moEst4s*");
+        String fraseHasheada = "931134d6275a8ea21cda129254e5a02d78021e532409b74276e11157a003f6ef";
+        assertEquals(hashfrase,fraseHasheada);
+
+    }
+
+    @Test
+    public void hastTestfaildConMinMayusNumsSibols(){
+        String hashfrase = Hashing.getSHA256Hash("!HolaC0moEst4s*");
+        String fraseHasheada = "931134d6275a8ea21cda129254e5a02d78021e5324319b72176e11157a003f6ef";
+        assertNotEquals(hashfrase,fraseHasheada);
+    }
+
+
+}
