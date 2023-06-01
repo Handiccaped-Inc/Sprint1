6ea1e4a036ff79a1f12f14c0064a0e514b4c5874
Jojan-Esteban-Serna <78891995+Jojan-Esteban-Serna@users.noreply.github.com>
2023-06-01 00:33:45 -0500

    Added docummentation and modified the language

--- payment-service/src/main/java/co/unicauca/payment/domain/service/TransactionService.java
+++ payment-service/src/main/java/co/unicauca/payment/domain/service/TransactionService.java
@@ -6,10 +6,21 @@ import co.unicauca.payment.domain.Transaction;
 public class TransactionService {
     private ITransactionRepository transactionRepository;
 
+    /**
+     * Constructor parametrizado
+     * 
+     * @param transactionRepository El repositorio a ser usado
+     */
     public TransactionService(ITransactionRepository transactionRepository) {
         this.transactionRepository = transactionRepository;
     }
 
+    /**
+     * Guarda una transaccion
+     * 
+     * @param newTransaction Transaccion a ser guardada
+     * @return True si la transaccion fue guardada, false de lo contrario
+     */
     public boolean save(Transaction newTransaction) {
         if (newTransaction.getDate() == null || newTransaction.getAmmount() <= 0
                 || newTransaction.getSender() == null || newTransaction.getReceiver() == null) {
