2155cf01894f68d768e6abd93c61bc1aa5e2d35e
SantiagoAV <81932949+SantiAV25@users.noreply.github.com>
2023-06-01 22:32:26 -0500

    feature/Santiago_Agredo/addDeliveryServiceTest

---
+++ openmarket-server/src/test/java/co/unicauca/openmarket/server/domain/service/DeliveryServiceTest.java
@@ -0,0 +1,115 @@
+package co.unicauca.openmarket.server.domain.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import co.unicauca.openmarket.commons.domain.Category;
+import co.unicauca.openmarket.commons.domain.Delivery;
+import co.unicauca.openmarket.commons.domain.Order;
+import co.unicauca.openmarket.commons.domain.Product;
+import co.unicauca.openmarket.commons.domain.Rol;
+import co.unicauca.openmarket.commons.domain.StateProduct;
+import co.unicauca.openmarket.commons.domain.StatusOrder;
+import co.unicauca.openmarket.commons.domain.User;
+import co.unicauca.openmarket.server.access.IDeliveryRepository;
+
+/**
+ * Test de la clase DeliveryService
+ * 
+ */
+
+public class DeliveryServiceTest {
+    IDeliveryRepository repository;
+    DeliveryService service;
+
+    public DeliveryServiceTest() {
+        repository = new mockDeliveryRepository();
+        service = new DeliveryService(repository);
+    }
+
+    @Test
+    public void TestsaveSucces(){
+            User usuario = new User(2L, new Rol(1L, "Comprador"), new Date(0), "Example2@email.com", "123454678", "1234567890", "nombre2", "usuarioDePrueba2", "12345", "calle-1");
+            Product product = new Product(2L, usuario, new Category(1L, "Juguetes"), new StateProduct(1L, "Disponible"), "Carro", "Es un carro", 1000L, 3L, 0, 0);
+            Order order = new Order(1L, usuario, product, new StatusOrder(1L,"Enviado"), (double) 100L,new Date(0), 3.0);
+            Delivery delivery = new Delivery(1l, order, usuario, usuario, new Date(0));
+            String responde = service.save(delivery);
+            assertEquals(responde, "ok");
+    } 
+
+    @Test
+    public void TestsaveFaild(){
+            User usuario = new User(2L, new Rol(1L, "Comprador"), new Date(0), "Example2@email.com", "123454678", "1234567890", "nombre2", "usuarioDePrueba2", "12345", "calle-1");
+            Product product = new Product(2L, usuario, new Category(1L, "Juguetes"), new StateProduct(1L, "Disponible"), "Carro", "Es un carro", 1000L, 3L, 0, 0);
+            Order order = null;
+            Delivery delivery = new Delivery(1l, order, usuario, usuario, new Date(0));
+            String responde = service.save(delivery);
+            assertTrue(responde.length() > 3);
+    } 
+
+    @Test
+    public void updateDeliverySucces(){
+            User usuario = new User(1L, new Rol(1L, "Comprador"), new Date(0), "Example@email.com", "123454678", "1234567890", "nombre", "usuarioDePrueba", "12345", "calle-1");
+            Product product = new Product(2L, usuario, new Category(1L, "Juguetes"), new StateProduct(1L, "Disponible"), "Carro", "Es un carro", 1000L, 3L, 0, 0);
+            Order order = new Order(3L, usuario, product, new StatusOrder(1L,"Devuelto"), (double) 100L,new Date(0), 3.0);
+            Delivery delivery = new Delivery(1l, order, usuario, usuario, new Date(0));
+            String responde = service.update(delivery);
+            assertEquals(responde, "ok");
+        
+        }
+
+        @Test
+         public void updateDeliveryfaild(){
+            User usuario = null;
+            Product product = new Product(2L, usuario, new Category(1L, "Juguetes"), new StateProduct(1L, "Disponible"), "Carro", "Es un carro", 1000L, 3L, 0, 0);
+            Order order = new Order(3L, usuario, product, new StatusOrder(1L,"Devuelto"), (double) 100L,new Date(0), 3.0);
+            Delivery delivery = new Delivery(1l, order, usuario, usuario, new Date(0));
+            String responde = service.update(delivery);
+            assertTrue(responde.length() > 3);
+        
+        }
+
+
+
+
+    private class mockDeliveryRepository implements IDeliveryRepository {
+        List<Delivery> deliveries;
+
+        public mockDeliveryRepository(){
+            deliveries = new ArrayList<>();
+            User usuario = new User(1L, new Rol(1L, "Comprador"), new Date(0), "Example@email.com", "123454678", "1234567890", "nombre", "usuarioDePrueba", "12345", "calle-1");
+            Product product = new Product(2L, usuario, new Category(1L, "Juguetes"), new StateProduct(1L, "Disponible"), "Carro", "Es un carro", 1000L, 3L, 0, 0);
+            Order order = new Order(3L, usuario, product, new StatusOrder(1L,"Enviado"), (double) 100L,new Date(0), 3.0);
+            Delivery delivery = new Delivery(1l, order, usuario, usuario, new Date(0));
+            deliveries.add(delivery);
+        }
+
+        @Override
+        public boolean save(Delivery newDelivery) {
+            // TODO Auto-generated method stub
+            return this.deliveries.add(newDelivery);
+        }
+
+        @Override
+        public boolean update(Delivery newDelivery) {
+            // TODO Auto-generated method stub
+            for (Delivery delivery : deliveries) {
+                if(delivery.getId() == newDelivery.getId()){
+                    delivery = newDelivery;
+                    return true;
+                }
+
+                }
+                return false;
+            }
+        }
+
+    }
+    
+
